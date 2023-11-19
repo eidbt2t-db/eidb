afe266c0737606a8ce2fcdd47bf0753e29025ed5
eidbt2t-db <151021598+eidbt2t-db@users.noreply.github.com>
2023-11-19 14:28:45 -0800

    updated content #2

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,16 @@
+// @ts-ignore
 import { defineCollection, z } from "astro:content";
 
+// @ts-ignore
 const showcase = defineCollection({
-  type: "data",
-  schema: ({ image }) =>
-    z.object({
-      title: z.string().min(1),
-      image: image(),
-      url: z.string().url(),
-      featured: z.number().min(1).optional(),
-    }),
+    schema: ({image}) =>
+        z.object({
+            title: z.string().min(1),
+            image: image(),
+            url: z.string().url(),
+            featured: z.number().min(1).optional(),
+        }),
+    type: "data",
 });
 
 export const collections = {
